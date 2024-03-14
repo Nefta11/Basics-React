57d10eac8bf1d818fd1dd8fb1857cd91b6e359a9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-14 09:07:07 -0600

    Updated

--- screens/StudentContext.js
+++ screens/StudentContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
-//El estado es un valor que el componente puede mantener, cambiar 
-//y usar para renderizar su interfaz de usuario. 
-
+//El estado es un valor que el componente puede mantener, cambiar
+//y usar para renderizar su interfaz de usuario.
 
 // Crea un Contexto para los Estudiantes. Este será el medio por el cual los componentes de React podrán acceder y manipular el estado de los estudiantes.
 const StudentContext = createContext();
@@ -16,20 +15,19 @@ export const StudentProvider = ({ children }) => {
     <StudentContext.Provider value={{ students, setStudents }}>
       {children}
     </StudentContext.Provider> // Renderiza los componentes hijos dentro del Proveedor del Contexto de Estudiantes. Estos componentes hijos serán capaces de acceder al estado de los estudiantes.
-
   );
 };
 
 // Crea un Hook personalizado para usar el Contexto de Estudiantes. Este hook será utilizado por los componentes para acceder al estado de los estudiantes.
 export const useStudents = () => {
-  // Utiliza el hook useContext para acceder al Contexto de Estudiantes.
+// Utiliza el hook useContext para acceder al Contexto de Estudiantes.
   const context = useContext(StudentContext);
 
-  // Si el contexto es indefinido (es decir, si useStudents se utiliza fuera de un Proveedor de Estudiantes), lanza un error.
+// Si el contexto es indefinido (es decir, si useStudents se utiliza fuera de un Proveedor de Estudiantes), lanza un error.
   if (context === undefined) {
     throw new Error("useStudents must be used within a StudentProvider");
   }
 
-  // Retorna el contexto, que incluye el estado de los estudiantes y la función para actualizar ese estado.
+// Retorna el contexto, que incluye el estado de los estudiantes y la función para actualizar ese estado.
   return context;
 };
